1b4938d0cc6ecdba242f7705b193411e78ba7e10
Алексей Шевченко <alex18w@mail.ru>
2022-07-20 20:19:31 +0500

    Сортировака массива методом максимального

---
+++ Massive_continue/Program.cs
@@ -0,0 +1,40 @@
+﻿// Алгортм сортировки методом выбора максимального (минимального)
+//Алгоритм сортировки методом мини-макса
+
+//берем первый элемент, находим минимальный в массиве и меняем их местами и т.д., таким образом отсортировываем массив
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+SelectionSort(arr);
+
+PrintArray(arr);
